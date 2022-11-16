7466a4fb1c023d3d3da62d62348050666386a735
technologicaljerry <cserajatverma@gmail.com>
2022-11-16 22:30:01 +0530

    uodate

---
+++ setTimeOut.js
@@ -0,0 +1,6 @@
+function theSetTimeout() {
+    console.log('Hello world, This is set time out');
+}
+
+setTimeout(theSetTimeout, 3000);
+console.log('The Second message will show after 3 Seconds');
