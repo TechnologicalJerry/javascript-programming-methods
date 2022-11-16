a70cc1aa6f535a689399a02a378941146aa3c7b0
technologicaljerry <cserajatverma@gmail.com>
2022-11-16 22:34:34 +0530

    update

--- setTimeOut.js
+++ setTimeOut.js
@@ -1,6 +1,11 @@
-function theSetTimeout() {
-    console.log('Hello world, This is set time out');
+function firstSetTimeout() {
+    console.log('Hello world, This is First set time out');
+}
+function secondSetTimeout() {
+    console.log('Hello world, This Second is set time out');
 }
 
-setTimeout(theSetTimeout, 3000);
+setTimeout(firstSetTimeout, 3000);
+setTimeout(secondSetTimeout, 6000);
 console.log('The Second message will show after 3 Seconds');
+console.log('The Third message will show after 6 Seconds');
