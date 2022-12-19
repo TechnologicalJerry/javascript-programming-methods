d5fa25d958a546a6c28029ad2232bff4d7ed6b6c
technologicaljerry <cserajatverma@gmail.com>
2022-12-19 21:17:39 +0530

    Implement join

---
+++ join().js
@@ -0,0 +1,3 @@
+let example2a = ["A", "B", "C", 1, 2, 3];
+let example2b = example2a.join(" | ");  
+console.log('result===>', example2b);
--- toString().js
+++ toString().js
@@ -1,7 +1,7 @@
 let text = "Hello World!";
 let result = text.toString();
-console.log('result', result);
+console.log('result===>', result);
 
 let example1a = ["A", "B", "C", 1, 2, 3];
 let example1b = example1a.toString();
-console.log('result', example1b);
+console.log('result===>', example1b);
