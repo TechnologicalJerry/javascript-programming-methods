34348f56ed53000b59f8c271848c771cc39069be
technologicaljerry <cserajatverma@gmail.com>
2022-12-19 21:23:33 +0530

    Create slice().js

---
+++ slice().js
@@ -0,0 +1,9 @@
+let example3a = ["A", "B", "C", 1, 2, 3];
+
+let example3b = example3a.splice(1);
+
+let example3c = example3a.splice(2, 5);
+
+console.log('A result===>', example3a);
+console.log('B result===>', example3b);
+console.log('C result===>', example3c);
