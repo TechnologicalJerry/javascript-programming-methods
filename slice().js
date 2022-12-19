8d79472e8c986a3711f60cd2cd29a02133d6f5df
technologicaljerry <cserajatverma@gmail.com>
2022-12-19 21:24:20 +0530

    Update slice().js

--- slice().js
+++ slice().js
@@ -1,9 +1,9 @@
-let example3a = ["A", "B", "C", 1, 2, 3];
+let first = ["A", "B", "C", 1, 2, 3];
 
-let example3b = example3a.splice(1);
+let second = first.splice(1);
 
-let example3c = example3a.splice(2, 5);
+let third = first.splice(2, 5);
 
-console.log('A result===>', example3a);
-console.log('B result===>', example3b);
-console.log('C result===>', example3c);
+console.log('A result===>', first);
+console.log('B result===>', second);
+console.log('C result===>', third);
