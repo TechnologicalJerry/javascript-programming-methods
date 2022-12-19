54ad9e7613510d10b5f8a5c15961500e39a1d80b
technologicaljerry <cserajatverma@gmail.com>
2022-12-19 21:15:55 +0530

    Create toString().js

---
+++ toString().js
@@ -0,0 +1,7 @@
+let text = "Hello World!";
+let result = text.toString();
+console.log('result', result);
+
+let example1a = ["A", "B", "C", 1, 2, 3];
+let example1b = example1a.toString();
+console.log('result', example1b);
