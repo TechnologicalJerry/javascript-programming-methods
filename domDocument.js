0aee17e5f494a3a2ba49546240c21924dcb677cc
technologicaljerry <cserajatverma@gmail.com>
2022-12-21 23:52:21 +0530

    Create domDocument.js

---
+++ domDocument.js
@@ -0,0 +1,8 @@
+document.getElementById(id);
+document.getElementsByTagName(name);
+document.getElementsByClassName(name);
+document.createElement(element);
+document.removeChild(element);
+document.appendChild(element);
+document.replaceChild(new old);
+document.write(text);
