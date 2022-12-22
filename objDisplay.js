9f0b28f7a5378c70b3bd61d44ff16110cfec23f6
technologicaljerry <cserajatverma@gmail.com>
2022-12-22 23:30:50 +0530

    Update objDisplay.js

--- objDisplay.js
+++ objDisplay.js
@@ -14,3 +14,17 @@ const secondPerson = {
 };
 
 document.getElementById("demo").innerHTML = person.name + ", " + person.age + ", " + person.city;
+
+
+const thirdPerson = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+let txt = "";
+for (let x in person) {
+    txt += person[x] + " ";
+};
+
+document.getElementById("demo").innerHTML = txt;
