0e23746358cb036a342b2dfec79dbc90d6fa6e6e
technologicaljerry <cserajatverma@gmail.com>
2022-12-22 23:30:05 +0530

    Update objDisplay.js

--- objDisplay.js
+++ objDisplay.js
@@ -1,7 +1,16 @@
-const person = {
+const pfirstPerson = {
     name: "John",
     age: 30,
     city: "New York"
 };
 
 document.getElementById("demo").innerHTML = person;
+
+
+const secondPerson = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+document.getElementById("demo").innerHTML = person.name + ", " + person.age + ", " + person.city;
