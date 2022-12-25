a5547d6f4c15b254abef6fe1ebc67e1cfc31cfa7
technologicaljerry <cserajatverma@gmail.com>
2022-12-25 22:02:52 +0530

    Update christmasTree.js

--- christmasTree.js
+++ christmasTree.js
@@ -7,3 +7,21 @@ function xmas(height) {
 }
 
 document.write(`<pre>${xmas(10)}</pre>`);
+
+
+var x = 8;
+for (let i = 0; i < x; i++) {
+    for (let j = x - 1; j > i; j--) {
+        document.write("&nbsp&nbsp");
+    }
+    for (let k = 0; k <= (i * 2); k++) {
+        document.write("^");
+    }
+    document.write("<br>");
+}
+for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < (x * 2) - 3; j++) {
+        document.write("&nbsp");
+    }
+    document.write("^<br>");
+}
