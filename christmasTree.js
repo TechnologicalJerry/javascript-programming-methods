877072a7e689f5f3525adb4d2a2e95984e2bccea
technologicaljerry <cserajatverma@gmail.com>
2022-12-25 22:02:21 +0530

    Update christmasTree.js

--- christmasTree.js
+++ christmasTree.js
@@ -1,10 +1,7 @@
 function xmas(height) {
-    // add 1 more level for the trunk, e.g. height+1
     return Array.from({ length: height + 1 }, (v, i) => {
         return i === height
-            // that's for the trunk of the tree
             ? '*'.padStart(Math.round((2 * i) / 2), ' ')
-            // the actual tree "levels"
             : '*'.repeat(2 * i + 1).padStart(2 * i + height - i, ' ');
     }).join('\n');
 }
