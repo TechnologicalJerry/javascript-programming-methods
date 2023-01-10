6bdb86a6458e8d1c1c4077149d12e645f8973617
technologicaljerry <cserajatverma@gmail.com>
2023-01-10 23:41:52 +0530

    Create SmallestPrime.js

---
+++ SmallestPrime.js
@@ -0,0 +1,17 @@
+function next_Prime_num(num) {
+    for (var i = num + 1; ; i++) {
+        var isPrime = true;
+        for (var d = 2; d * d <= i; d++) {
+            if (i % d === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            return i;
+        }
+    }
+}
+
+console.log(next_Prime_num(3));
+console.log(next_Prime_num(17));
