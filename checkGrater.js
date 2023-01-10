36a146c70e46c8c578bcf5e7e3da12a98228143e
technologicaljerry <cserajatverma@gmail.com>
2023-01-10 23:33:55 +0530

    Create checkGrater.js

---
+++ checkGrater.js
@@ -0,0 +1,9 @@
+function test_fifteen(num) {
+    while (num < 15) {
+        num++;
+    }
+    return num;
+}
+console.log(test_fifteen("123"));
+console.log(test_fifteen("10"));
+console.log(test_fifteen("5"));
