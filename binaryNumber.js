e7a809b445c42f4654772cc522ddecb93ab2623a
technologicaljerry <cserajatverma@gmail.com>
2023-01-16 23:42:02 +0530

    Create binaryNumber.js

---
+++ binaryNumber.js
@@ -0,0 +1,10 @@
+const test = (n) => {
+    if (n === 0 || n === 1) {
+        return String(n)
+    }
+    return test(Math.floor(n / 2)) + String(n % 2)
+}
+console.log(test(1))
+console.log(test(0))
+console.log(test(10))
+console.log(test(101))
