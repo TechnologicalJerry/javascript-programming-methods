f60fed1e22826f29e4950a5a8a40088e215c090f
technologicaljerry <cserajatverma@gmail.com>
2023-01-19 23:13:09 +0530

    Create sumOfValues.js

---
+++ sumOfValues.js
@@ -0,0 +1,18 @@
+function sum(input){
+             
+    if (toString.call(input) !== "[object Array]")
+       return false;
+         
+               var total =  0;
+               for(var i=0;i<input.length;i++)
+                 {                  
+                   if(isNaN(input[i])){
+                   continue;
+                    }
+                     total += Number(input[i]);
+                  }
+                return total;
+               }
+   console.log(sum([1,2,3]));
+   console.log(sum([100,-200,3]));
+   console.log(sum([1,2,'a',3]));
