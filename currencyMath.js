c1a9e48b16fcf3d47c9a13d9565b926856969a3a
technologicaljerry <cserajatverma@gmail.com>
2023-01-21 22:56:07 +0530

    Update currencyMath.js

--- currencyMath.js
+++ currencyMath.js
@@ -2,10 +2,10 @@ var n1 = '$40.24';
 var n2 = '$21.57';
 var regp = /[^0-9.-]+/g;
 
-console.log(parseFloat(n1.replace(regp, '')) +       parseFloat(n2.replace(regp, '')));
+console.log(parseFloat(n1.replace(regp, '')) + parseFloat(n2.replace(regp, '')));
 
-console.log(parseFloat(n1.replace(regp, '')) -         parseFloat(n2.replace(regp, '')));
+console.log(parseFloat(n1.replace(regp, '')) - parseFloat(n2.replace(regp, '')));
 
-console.log(parseFloat(n1.replace(regp, '')) *         parseFloat(n2.replace(regp, '')));
+console.log(parseFloat(n1.replace(regp, '')) * parseFloat(n2.replace(regp, '')));
 
-console.log(parseFloat(n1.replace(regp, '')) /         parseFloat(n2.replace(regp, '')));
+console.log(parseFloat(n1.replace(regp, '')) / parseFloat(n2.replace(regp, '')));
