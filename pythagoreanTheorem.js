3c05aafeb3bd52b06bf74deccbfd3a0a9c8e473e
technologicaljerry <cserajatverma@gmail.com>
2023-01-23 22:39:52 +0530

    Update pythagoreanTheorem.js

--- pythagoreanTheorem.js
+++ pythagoreanTheorem.js
@@ -1,5 +1,5 @@
-function pythagorean(sideA, sideB){
+function pythagorean(sideA, sideB) {
     return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
-  }
-  
-  console.log(pythagorean(4, 3));
+}
+
+console.log(pythagorean(4, 3));
