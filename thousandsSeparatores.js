b538ef78b3ad2003a10953d69ad5b188c345fc5a
technologicaljerry <cserajatverma@gmail.com>
2023-01-23 22:54:57 +0530

    Create thousandsSeparatores.js

---
+++ thousandsSeparatores.js
@@ -0,0 +1,10 @@
+function thousands_separators(num)
+  {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+  }
+
+console.log(thousands_separators(1000));
+console.log(thousands_separators(10000.23));
+console.log(thousands_separators(100000));
