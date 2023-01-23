7384705ca6806fbaee569051061c20748380f272
technologicaljerry <cserajatverma@gmail.com>
2023-01-23 22:41:14 +0530

    Update powerOfTwo.js

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -1,7 +1,6 @@
-function isPower_of_two(num)
-  {
-     return num && (num & (num - 1)) === 0;
+function isPower_of_two(num) {
+    return num && (num & (num - 1)) === 0;
 }
 
-console.log(isPower_of_two(64));  
+console.log(isPower_of_two(64));
 console.log(isPower_of_two(94));
