d2b7d38c0ba19e98622ac87da06e3fa5c62bcce4
technologicaljerry <cserajatverma@gmail.com>
2023-01-27 21:37:53 +0530

    Create randomBGColor.js

---
+++ randomBGColor.js
@@ -0,0 +1,11 @@
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    }
+
+random_bg_color();
