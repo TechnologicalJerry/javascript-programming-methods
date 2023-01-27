4f4921ff251a9125f80034812b87c27a0b49d61f
technologicaljerry <cserajatverma@gmail.com>
2023-01-27 21:38:03 +0530

    Update randomBGColor.js

--- randomBGColor.js
+++ randomBGColor.js
@@ -3,9 +3,9 @@ function random_bg_color() {
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
- console.log(bgColor);
-  
+    console.log(bgColor);
+
     document.body.style.background = bgColor;
-    }
+}
 
 random_bg_color();
