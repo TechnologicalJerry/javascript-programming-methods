0b9472ab8c80294294347c08fcc8464b9583270c
technologicaljerry <cserajatverma@gmail.com>
2023-01-27 21:50:19 +0530

    Update primeNoFromZero.js

--- primeNoFromZero.js
+++ primeNoFromZero.js
@@ -1,13 +1,9 @@
-function primeFactorsTo(max)
-{
-    var store  = [], i, j, primes = [];
-    for (i = 2; i <= max; ++i) 
-    {
-        if (!store [i]) 
-          {
+function primeFactorsTo(max) {
+    var store = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!store[i]) {
             primes.push(i);
-            for (j = i << 1; j <= max; j += i) 
-            {
+            for (j = i << 1; j <= max; j += i) {
                 store[j] = true;
             }
         }
