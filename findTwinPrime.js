87cba3fc3efd9f10e09e599fbfaedbb740d8f4e5
technologicaljerry <cserajatverma@gmail.com>
2023-02-01 23:50:40 +0530

    Update findTwinPrime.js

--- findTwinPrime.js
+++ findTwinPrime.js
@@ -2,13 +2,13 @@ function twin_Prime (n) {
     const prime_num = test_prime(n)
   
     if (!prime_num) {
-      return -1
+      return -1;
     }
     if (!test_prime(n + 2)) {
-      return -1
+      return -1;
     }
   
-    return n + 2
+    return n + 2;
   }
   function test_prime(n)
   {
@@ -32,7 +32,7 @@ function twin_Prime (n) {
       return true;  
     }
   }
-  console.log("Twin prime of 5 is:"+twin_Prime(5))
-  console.log("Twin prime of 59 is:"+twin_Prime(59))
-  console.log("Twin prime of 107 is:"+twin_Prime(107))
-  console.log("Twin prime of 61 is:"+twin_Prime(61))
+  console.log("Twin prime of 5 is:"+twin_Prime(5));
+  console.log("Twin prime of 59 is:"+twin_Prime(59));
+  console.log("Twin prime of 107 is:"+twin_Prime(107));
+  console.log("Twin prime of 61 is:"+twin_Prime(61));
