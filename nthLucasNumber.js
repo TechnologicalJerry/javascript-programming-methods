d523a214b3d6cb49ca780d13779e47b57984d9d6
technologicaljerry <cserajatverma@gmail.com>
2023-02-01 23:48:52 +0530

    Update nthLucasNumber.js

--- nthLucasNumber.js
+++ nthLucasNumber.js
@@ -2,17 +2,17 @@ function find_lucas_number(index) {
     if (index >= 0)
       {
       if (Math.floor(index) !== index) throw new TypeError('Index cannot be a Decimal')
-      let x = 2
-      let y = 1
+      let x = 2;
+      let y = 1;
       for (let i = 0; i < index; i++) {
-        const temp = x + y
-        x = y
-        y = temp
+        const temp = x + y;
+        x = y;
+        y = temp;
       }
-      return x
+      return x;
     }
   }
-  console.log(find_lucas_number(0))
-  console.log(find_lucas_number(4))
-  console.log(find_lucas_number(7))
-  console.log(find_lucas_number(5.4))
+  console.log(find_lucas_number(0));
+  console.log(find_lucas_number(4));
+  console.log(find_lucas_number(7));
+  console.log(find_lucas_number(5.4));
