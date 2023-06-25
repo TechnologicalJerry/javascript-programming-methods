53adbaf2b43bf8ff45b5e99bb3832ea3fa2d92c3
technologicaljerry <cserajatverma@gmail.com>
2023-06-25 23:46:18 +0530

    Update objDisplay.js

--- objDisplay.js
+++ objDisplay.js
@@ -44,3 +44,16 @@ const thePerson = {
 };
 
 document.getElementById("demo").innerHTML = thePerson.name + ", " + thePerson.age + ", " + thePerson.city;
+
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+  
+  let txt = "";
+  for (let x in person) {
+  txt += person[x] + " ";
+  };
+  
+  document.getElementById("demo").innerHTML = txt;
