63c89d8e04684029b0acecf7bdc38df052fd5c78
technologicaljerry <cserajatverma@gmail.com>
2023-06-25 23:49:09 +0530

    Update objDisplay.js

--- objDisplay.js
+++ objDisplay.js
@@ -58,13 +58,13 @@ document.getElementById("demo").innerHTML = thePerson.name + ", " + thePerson.ag
 
 // document.getElementById("demo").innerHTML = txt;
 
-const person = {
-    name: "John",
-    age: 30,
-    city: "New York"
-};
+// const person = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// };
 
-const myArray = Object.values(person);
+// const myArray = Object.values(person);
 
 // const person = {
 //     name: "John",
@@ -74,3 +74,11 @@ const myArray = Object.values(person);
 
 // const myArray = Object.values(person);
 // document.getElementById("demo").innerHTML = myArray;
+
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+let myString = JSON.stringify(person);
