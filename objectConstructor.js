7f8bf8d43f43c232292880f17c588e47c5299b10
technologicaljerry <cserajatverma@gmail.com>
2023-06-26 23:44:07 +0530

    Update objectConstructor.js

--- objectConstructor.js
+++ objectConstructor.js
@@ -54,6 +54,29 @@
 // document.getElementById("demo").innerHTML =
 //     "My father is " + myFather.nationality; 
 
+// // Constructor function for Person objects
+// function Person(first, last, age, eye) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+// }
+
+// // Create 2 Person objects
+// const myFather = new Person("John", "Doe", 50, "blue");
+// const myMother = new Person("Sally", "Rally", 48, "green");
+
+// // Add a name method to first object
+// myFather.name = function () {
+//     return this.firstName + " " + this.lastName;
+// };
+
+// // Display full name
+// document.getElementById("demo").innerHTML =
+//     "My father is " + myFather.name(); 
+
+
+
 // Constructor function for Person objects
 function Person(first, last, age, eye) {
     this.firstName = first;
@@ -62,15 +85,13 @@ function Person(first, last, age, eye) {
     this.eyeColor = eye;
 }
 
+// You can NOT add a new property to a constructor function
+Person.nationality = "English";
+
 // Create 2 Person objects
 const myFather = new Person("John", "Doe", 50, "blue");
 const myMother = new Person("Sally", "Rally", 48, "green");
 
-// Add a name method to first object
-myFather.name = function () {
-    return this.firstName + " " + this.lastName;
-};
-
-// Display full name
+// Display nationality
 document.getElementById("demo").innerHTML =
-    "My father is " + myFather.name(); 
+    "The nationality of my father is " + myFather.nationality; 
