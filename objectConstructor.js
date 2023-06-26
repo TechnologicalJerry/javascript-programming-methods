ceabc710396f2c25ffa2770a327edeb018a231ea
technologicaljerry <cserajatverma@gmail.com>
2023-06-26 23:42:08 +0530

    Update objectConstructor.js

--- objectConstructor.js
+++ objectConstructor.js
@@ -1,3 +1,20 @@
+// // Constructor function for Person objects
+// function Person(first, last, age, eye) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+// }
+
+// // Create a Person object
+// const myFather = new Person("John", "Doe", 50, "blue");
+
+// // Display age
+// document.getElementById("demo").innerHTML =
+//     "My father is " + myFather.age + ".";
+
+
+
 // Constructor function for Person objects
 function Person(first, last, age, eye) {
     this.firstName = first;
@@ -6,12 +23,10 @@ function Person(first, last, age, eye) {
     this.eyeColor = eye;
 }
 
-// Create a Person object
+// Create two Person objects
 const myFather = new Person("John", "Doe", 50, "blue");
+const myMother = new Person("Sally", "Rally", 48, "green");
 
 // Display age
 document.getElementById("demo").innerHTML =
-    "My father is " + myFather.age + ".";
-
-
-
+    "My father is " + myFather.age + ". My mother is " + myMother.age + "."; 
