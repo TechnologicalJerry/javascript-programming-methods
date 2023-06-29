d03378a495b5b8010376c8195bebf669fcaf2c6a
technologicaljerry <cserajatverma@gmail.com>
2023-06-29 23:29:48 +0530

    Update objectConstructor.js

--- objectConstructor.js
+++ objectConstructor.js
@@ -47,10 +47,8 @@
 // const myFather = new Person("John", "Doe", 50, "blue");
 // const myMother = new Person("Sally", "Rally", 48, "green");
 
-// // Add nationality to first object
 // myFather.nationality = "English";
 
-// // Display nationality 
 // document.getElementById("demo").innerHTML =
 //     "My father is " + myFather.nationality; 
 
