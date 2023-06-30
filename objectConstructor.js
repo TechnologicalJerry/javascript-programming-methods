40c9dfb78d76519543cd323e4094fe516c361913
technologicaljerry <cserajatverma@gmail.com>
2023-06-30 23:56:47 +0530

    Update objectConstructor.js

--- objectConstructor.js
+++ objectConstructor.js
@@ -125,3 +125,18 @@
 // }
 
 // person.fullName.call(person1, "Oslo", "Norway");
+
+const person = {
+  firstName:"John",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+const member = {
+  firstName:"Hege",
+  lastName: "Nilsen",
+}
+
+let fullName = person.fullName.bind(member);
