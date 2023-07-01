0923173d2039df6689aac7b90b165835d99a2688
technologicaljerry <cserajatverma@gmail.com>
2023-07-01 23:58:57 +0530

    Update objectConstructor.js

--- objectConstructor.js
+++ objectConstructor.js
@@ -36,21 +36,6 @@
 //     this.eyeColor = eye;
 // }
 
-// const myFather = new Person("John", "Doe", 50, "blue");
-// const myMother = new Person("Sally", "Rally", 48, "green");
-
-// myFather.nationality = "English";
-
-// document.getElementById("demo").innerHTML =
-//     "My father is " + myFather.nationality;
-
-// function Person(first, last, age, eye) {
-//     this.firstName = first;
-//     this.lastName = last;
-//     this.age = age;
-//     this.eyeColor = eye;
-// }
-
 
 
 
