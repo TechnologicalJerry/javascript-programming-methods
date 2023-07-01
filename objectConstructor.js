91eb7d3487c6823ebcd394f4b42988e110959111
technologicaljerry <cserajatverma@gmail.com>
2023-07-01 23:57:56 +0530

    Update objectConstructor.js

--- objectConstructor.js
+++ objectConstructor.js
@@ -163,14 +163,3 @@
 
 // setTimeout(person.display, 3000);
 
-// const person = {
-//     firstName: "John",
-//     lastName: "Doe",
-//     display: function () {
-//         let x = document.getElementById("demo");
-//         x.innerHTML = this.firstName + " " + this.lastName;
-//     }
-// }
-
-// let display = person.display.bind(person);
-// setTimeout(display, 3000);
