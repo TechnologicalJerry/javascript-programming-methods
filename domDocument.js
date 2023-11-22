b3edf34a0b09f62dd0d3707071799ee5a598784c
technologicaljerry <cserajatverma@gmail.com>
2023-11-22 23:44:26 +0530

    Update domDocument.js

--- domDocument.js
+++ domDocument.js
@@ -5,6 +5,7 @@ document.getElementsByTagName(name);
 document.getElementsByClassName(name);
 
 document.createElement(element);
+
 document.removeChild(element);
 document.appendChild(element);
 document.replaceChild(new old);
