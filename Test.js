b62369f0ffd5acbab1188eb18649a8cab1d44351
technologicaljerry <cserajatverma@gmail.com>
2023-11-25 23:52:41 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -3,7 +3,7 @@
 
 
 
-let myArray = ["Fan","Plus",35, null, true, false];
+let myArray = ["Fan", "Plus", 35, null, true, false];
 console.log(myArray.length);
 // myArray.pop();
 // myArray.push("Jerry");
