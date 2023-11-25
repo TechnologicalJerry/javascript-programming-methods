233ca8a00cdd03b268cced6c9f6e9fa09fc0bef2
technologicaljerry <cserajatverma@gmail.com>
2023-11-25 23:52:24 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -6,24 +6,6 @@
 
 
 
-// console.log("Loops");
-// let theArray = [1, 2, 3, 4, 5, 6, 7];
-// console.log(theArray);
-// for (var i = 0; i < theArray.length; i++) {
-//     console.log(theArray[i]);
-// }
-// theArray.forEach(function (element) {
-//     console.log(element);
-// });
-// let j = 10;
-// while (j < theArray.length) {
-//     console.log(theArray[j]);
-//     j++;
-// }
-// do {
-//     console.log(theArray[j]);
-//     j++;
-// } while (j < theArray.length);
 
 // let theArray = [1, 2, 3, 4, 5, 6, 7];
 // for (var i = 0; i < theArray.length; i++) {
