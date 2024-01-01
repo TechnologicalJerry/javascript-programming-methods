ff5e9ac3dc731879a8c8b4336a96df79eb39dc04
technologicaljerry <cserajatverma@gmail.com>
2024-01-01 22:42:34 +0530

    Delete checkGrater.js

--- checkGrater.js
+++
@@ -1,9 +0,0 @@
-function test_fifteen(num) {
-    while (num < 15) {
-        num++;
-    }
-    return num;
-}
-console.log(test_fifteen("123"));
-console.log(test_fifteen("10"));
-console.log(test_fifteen("5"));
