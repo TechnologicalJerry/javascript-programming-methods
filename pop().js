50a47e36af52ac3ece48c04c18a50003c3d8299e
Rajat Verma <cserajatverma@gmail.com>
2024-01-04 11:38:44 +0530

    Delete pop().js

--- pop().js
+++
@@ -1,8 +0,0 @@
-let theArray = ["A", "B", "C", "D"];
-let popOutput = theArray.pop();  
-console.log('A Result===>', popOutput);
-
-
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-let removed = fruits.pop();
-console.log('B Result===>', removed);
